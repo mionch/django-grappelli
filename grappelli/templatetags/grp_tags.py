c49d056cc43d36c264a5516868b60d1dc19a4ad5
mdzaman <michal@ordergroup.pl>
2015-04-14 13:51:52 +0200

    Added support for collapsible list-view column headers with tooltips containing standard column name.

--- grappelli/templatetags/grp_tags.py
+++ grappelli/templatetags/grp_tags.py
@@ -4,6 +4,11 @@
 from functools import wraps
 
 # try to use json (2.6+) but stay compatible with 2.5.*
+from django.contrib.admin.templatetags.admin_list import result_list, result_hidden_fields, results
+from django.contrib.admin.util import label_for_field
+from django.contrib.admin.views.main import ORDER_VAR
+from django.utils.html import format_html
+
 try:
     import json
 except ImportError:
@@ -221,6 +226,108 @@ def admin_list_filter(cl, spec):
 @register.simple_tag
 def admin_list_filter_custom(cl, spec):
     return admin_list_filter(cl, spec)
+
+
+def extended_result_headers(cl):
+    """
+    Generates the list column headers taking into account collapsible header texts.
+    """
+    ordering_field_columns = cl.get_ordering_field_columns()
+    for i, field_name in enumerate(cl.list_display):
+        text, attr = label_for_field(field_name, cl.model, model_admin=cl.model_admin, return_attr=True)
+        shortened_name = cl.model_admin.list_shortened_labels.get(field_name, None)
+        tooltip = text if shortened_name else ''
+        if shortened_name:
+            text = shortened_name
+
+        if attr:
+            # Potentially not sortable
+            # if the field is the action checkbox: no sorting and special class
+            if field_name == 'action_checkbox':
+                yield {
+                    "text": text,
+                    "tooltip": tooltip,
+                    "class_attrib": mark_safe(' class="action-checkbox-column"'),
+                    "sortable": False,
+                }
+                continue
+
+            admin_order_field = getattr(attr, "admin_order_field", None)
+            if not admin_order_field:
+                # Not sortable
+                yield {
+                    "text": text,
+                    "class_attrib": format_html(' class="column-{0}"', field_name),
+                    "sortable": False,
+                }
+                continue
+
+        # OK, it is sortable if we got this far
+        th_classes = ['sortable', 'column-{0}'.format(field_name)]
+        order_type = ''
+        new_order_type = 'asc'
+        sort_priority = 0
+        sorted = False
+        # Is it currently being sorted on?
+        if i in ordering_field_columns:
+            sorted = True
+            order_type = ordering_field_columns.get(i).lower()
+            sort_priority = list(ordering_field_columns).index(i) + 1
+            th_classes.append('sorted %sending' % order_type)
+            new_order_type = {'asc': 'desc', 'desc': 'asc'}[order_type]
+
+        # build new ordering param
+        o_list_primary = []  # URL for making this field the primary sort
+        o_list_remove = []  # URL for removing this field from sort
+        o_list_toggle = []  # URL for toggling order type for this field
+        make_qs_param = lambda t, n: ('-' if t == 'desc' else '') + str(n)
+
+        for j, ot in ordering_field_columns.items():
+            if j == i:  # Same column
+                param = make_qs_param(new_order_type, j)
+                # We want clicking on this header to bring the ordering to the
+                # front
+                o_list_primary.insert(0, param)
+                o_list_toggle.append(param)
+                # o_list_remove - omit
+            else:
+                param = make_qs_param(ot, j)
+                o_list_primary.append(param)
+                o_list_toggle.append(param)
+                o_list_remove.append(param)
+
+        if i not in ordering_field_columns:
+            o_list_primary.insert(0, make_qs_param(new_order_type, i))
+
+        yield {
+            "text": text,
+            "tooltip": tooltip,
+            "sortable": True,
+            "sorted": sorted,
+            "ascending": order_type == "asc",
+            "sort_priority": sort_priority,
+            "url_primary": cl.get_query_string({ORDER_VAR: '.'.join(o_list_primary)}),
+            "url_remove": cl.get_query_string({ORDER_VAR: '.'.join(o_list_remove)}),
+            "url_toggle": cl.get_query_string({ORDER_VAR: '.'.join(o_list_toggle)}),
+            "class_attrib": format_html(' class="{0}"', ' '.join(th_classes)) if th_classes else '',
+        }
+
+
+@register.inclusion_tag("admin/change_list_results.html")
+def result_list_custom(cl):
+    """
+    Displays the headers and data list together
+    """
+    headers = list(extended_result_headers(cl))
+    num_sorted_fields = 0
+    for h in headers:
+        if h['sortable'] and h['sorted']:
+            num_sorted_fields += 1
+    return {'cl': cl,
+            'result_hidden_fields': list(result_hidden_fields(cl)),
+            'result_headers': headers,
+            'num_sorted_fields': num_sorted_fields,
+            'results': list(results(cl))}
 
 
 @register.simple_tag(takes_context=True)
